bfbbb8ed72d3d92968a0d6d7507dfb2a55a6f357
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-10-20 12:32:52 +0200

    fix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.port || 3000,
+    port: process.env.PORT || 3000,
     database: 'mongodb+srv://rostik19wozniak:LSOMx26CE5xTBZWS@cluster0.rukr7wi.mongodb.net',
 }
